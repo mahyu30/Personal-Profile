816877a2b735c87e045f08cba683109915001bd9
kloydmatyo <kloydmatyo@gmail.com>
2025-09-02 19:18:00 +0800

    added animation in project section

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const colorBtn = document.getElementById("colorBtn");
     const bio = document.getElementById("bio");
+    const toggleProjectsBtn = document.getElementById("toggleProjectsBtn");
+    const projectsSection = document.querySelector('.projects-section');
 
     colorBtn.addEventListener("click", function() {
         // Change bio color
         bio.style.color = bio.style.color === "teal" ? "#333" : "teal";
+    });
+
+    toggleProjectsBtn.addEventListener("click", function() {
+        projectsSection.classList.toggle("hide");
     });
 });
