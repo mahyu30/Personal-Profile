e919fabf2a606544b7032527d869d53ddda7bd2b
kloydmatyo <kloydmatyo@gmail.com>
2025-09-05 09:55:39 +0800

    changed bio change color to name change color

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", function() {
     alert("Welcome to Cloyd Matthew Arabe's personal profile!");
 
     const colorBtn = document.getElementById("colorBtn");
-    const bio = document.getElementById("bio");
+    const name = document.getElementById("name");
     const toggleProjectsBtn = document.getElementById("toggleSkillsBtn");
     const skillsSection = document.querySelector('.skills-section');
     const profileCard = document.querySelector('.profile-card');
 
     colorBtn.addEventListener("click", function() {
-        // Change bio color
-        bio.style.color = bio.style.color === "teal" ? "#333" : "teal";
+        // Change name color
+        name.style.color = name.style.color === "teal" ? "#333" : "teal";
     });
 
     toggleProjectsBtn.addEventListener("click", function() {
